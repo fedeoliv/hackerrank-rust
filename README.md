e5071d108adf943cd123d26dd2e0cb37d7c25bdb
Fernando de Oliveira <5161098+fernandoBRS@users.noreply.github.com>
2021-07-13 15:58:51 -0300

    feature: Super Reduced String

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# hackerrank-rust
+# HackerRank solutions in Rust
+
+## Algorithms
+
+| Type | Solved problems |
+------ | --------------- |
+| String | <ul><li>[Super Reduced String](./algorithms/strings/super-reduced-string/src/main.rs)</li></ul> |
--- algorithms/strings/super-reduced-string/src/main.rs
+++ algorithms/strings/super-reduced-string/src/main.rs
@@ -1,3 +1,65 @@
+// Problem: https://www.hackerrank.com/challenges/reduced-string/problem
+
+use std::io;
+
+fn super_reduced_string(input: String) {
+    if input.is_empty() {
+        println!("Empty String");
+        return;
+    }
+
+    let input_chars: Vec<char> = input.chars().collect();
+    let input_size: usize = input.len();
+    let mut found_adjacent_letters: bool = false;
+
+    for i in 0..(input_size - 1) {
+        if input_chars[i] == input_chars[i + 1] {
+            let result: String = reduce_string(&input_chars, input_size, i);
+
+            super_reduced_string(result);
+            found_adjacent_letters = true;
+            break;
+        }
+    }
+
+    if !found_adjacent_letters {
+        println!("{}", input);
+    }
+}
+
+fn reduce_string(input_chars: &Vec<char>, input_size: usize, index: usize) -> String {
+    let mut result = String::new();
+    let left_input: String = substring(&input_chars, 0, index);
+    let right_input: String = substring(&input_chars, index + 2, input_size);
+
+    result.push_str(&left_input);
+    result.push_str(&right_input);
+
+    result
+}
+
+fn substring(input: &Vec<char>, start_index: usize, end_index: usize) -> String {
+    let mut result = String::new();
+
+    for i in start_index..end_index {
+        result.push(input[i]);
+    }
+
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input: String = read_line();
+    super_reduced_string(input);
+}
+
+fn read_line() -> String {
+    let mut buffer = String::new();
+    let reader: io::Stdin = io::stdin();
+
+    reader
+        .read_line(&mut buffer)
+        .expect("Could not read stdin!");
+
+    buffer.trim().to_string()
 }
